537550ee6c14b378b23129e8e137b98cdfbc9744
shownola <swasieko@gmail.com>
2020-06-09 14:05:45 -0700

    Updated styling

--- README.md
+++ README.md
@@ -12,6 +12,27 @@ Things you may want to cover:
 
 * Deployment instructions
 * Need to add redis and get production ready
-* 
+*
 
 * ...
+
+
+
+
+Detecting rails configuration failed
+remote:        set HEROKU_DEBUG_RAILS_RUNNER=1 to debug
+
+
+4 Eaches Skinless, Boneless Chicken Breast Halves
+1 Pinch Salt And Freshly Ground Black Pepper To Taste
+2 Eggs
+1 Cup Panko Bread Crumbs, Or More As Needed
+½ Cup Grated Parmesan Cheese
+2 Tablespoons All Purpose Flour, Or More If Needed
+1 Cup Olive Oil For Frying
+½ Cup Prepared Tomato Sauce
+¼ Cup Fresh Mozzarella, Cut Into Small Cubes
+¼ Cup Chopped Fresh Basil
+½ Cup Grated Provolone Cheese
+¼ Cup Grated Parmesan Cheese
+1 Tablespoon Olive Oil
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -16,15 +16,15 @@ CarrierWave.configure do |config|
 end
 
 
-# # if Rails.env.production?
-#   CarrierWave.configure do |config|
-#       config.fog_credentials = {
-#         :provider => 'AWS',
-#         :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-#         :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-#         :region                => ENV['S3_REGION']
-#       }
-#       config.fog_directory = ENV['S3_bucket']
-#       config.storage       = :fog
-#     end
-#   # end
+ # if Rails.env.production?
+ #  CarrierWave.configure do |config|
+ #      config.fog_credentials = {
+ #        :provider => 'AWS',
+ #        :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+ #        :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+ #        :region                => ENV['S3_REGION']
+ #      }
+ #      config.fog_directory = ENV['S3_bucket']
+ #      config.storage       = :fog
+ #    end
+ #   end
